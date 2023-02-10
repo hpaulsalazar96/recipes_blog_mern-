6faa5f2859b2dc3273410b64b6e42353438cc435
Henry Salazar <hsalazar@estud.usfq.edu.ec>
2023-02-09 23:58:12 -0500

    list fontawesome

--- app_server/controllers/users.js
+++ app_server/controllers/users.js
@@ -1,7 +1,33 @@
 //controllers
 
+let usuarios = [
+    {
+        nombre: 'Jorge',
+        apellido: 'Mian',
+        direccion:"Quito",
+    },
+    {
+        nombre: 'Tom',
+        apellido: 'QW',
+        direccion:"Quito",
+    },
+    {
+        nombre: 'Felipe',
+        apellido: 'Asd',
+        direccion:"Quito",
+    },
+    {
+        nombre: 'Pedro',
+        apellido: 'Verna',
+        direccion:"Quito",
+    },
+]
+
 const index = (req, res, next) => {
-    res.render('users', { title: 'Users' });
+    res.render('users', { 
+        title: 'Users',
+        usuarios
+    });
   }
 
 const userCreate = (req, res, next) => {
