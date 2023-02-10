a956fb8a562418b536e723301dd59559d2296306
Henry Salazar <hsalazar@estud.usfq.edu.ec>
2023-02-10 08:32:24 -0500

    config db

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+// modelo
+require('./app_server/models/db');
+
 // routes
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
---
+++ app_server/models/db.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const readLine = require('readline');
+
+if(process.platform === 'win32'){
+    const rl = readLine.Interface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.on('SIGINT', ()=>{
+        process.emit('SIGINT');
+    });
+}
+
+const procShutdown = (msg, callback) =>{
+    mongoose.connection.close(()=>{
+        console.log('Mongoose se desconecto a traves de: ',msg);
+        callback()
+    });
+}
+
+process.on('SIGUSR2', ()=>{
+    procShutdown('terminado por heroku', () =>{
+        process.kill(process.pid, 'SIGUSR2');
+    })
+})
+
+process.on('SIGTERM', ()=>{
+    procShutdown('terminado por heroku', () =>{
+        process.exit(0);
+    })
+})
+
+process.on('SIGINT', ()=>{
+    procShutdown('terminado por windows', () =>{
+        process.exit(0);
+    })
+})
+
+const dbURI = 'mongodb://localhost:27017/db_recipes_project';
+mongoose.set('strictQuery', true)
+
+// Connect MongoDB at default port 27017.
+mongoose.connect(dbURI, {
+    serverSelectionTimeoutMS: 5000,
+    family: 4,
+}).catch(err => console.log('Mongoose Error: ',err.reason));
+
+mongoose.connection.on('connected',()=>{
+    console.log(`Mongoose se conecto a: ${dbURI}`);
+});
+
+mongoose.connection.on('disconnected',()=>{
+    console.log(`Mongoose se desconecto de: ${dbURI}`);
+});
